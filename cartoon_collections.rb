f42f008d8317ff36031879e7ed75df31ed0f4e36
Rob Parker <into.the.parker@gmail.com>
2018-08-31 08:28:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  output = array.collect do |capt|
+     capt.upcase << "!"
+   end
+   output
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.size > 4
+  end
 end
-
-def find_the_cheese# code an argument here
+ 
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+  return array.include?(cheese)
+  end
+  array.find do |cheese|
+    cheese.include?(cheese_types)
+  end
+  
 end
